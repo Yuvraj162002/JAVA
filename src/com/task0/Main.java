dd61c8a6b89ebbe0fc1741df0132fe4c021162ca
Yuvraj@162002 <yuvrajpareek44@gmail.com>
2021-03-26 11:45:19 -0700

    First task

---
+++ src/com/task0/Main.java
@@ -0,0 +1,12 @@
+package com.task0;
+import  java .util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("enter the value of integer");
+        Scanner sc = new Scanner(System.in);
+        int  a = sc.nextInt();
+        System.out.println(a);
+    }
+}
