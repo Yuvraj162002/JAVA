fa9ca44966a3eaa19fba5550feaf78e83303e4f4
Yuvraj@162002 <yuvrajpareek44@gmail.com>
2021-03-27 10:44:36 -0700

    Ninth task

---
+++ src/com/task0/LargestAmongThreeNumber.java
@@ -0,0 +1,28 @@
+package com.task0;
+
+import java.util.Scanner;
+
+public class LargestAmongThreeNumber {
+    public static void main(String[] args) {
+        System.out.println("Enter  number1");
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        System.out.println("Enter  number2");
+        int n2 = sc.nextInt();
+
+        System.out.println("Enter  number3");
+        int n3= sc.nextInt();
+
+        if((n1>n2)&&(n2>n3)){
+            System.out.println("n1 is greater");
+        }
+        else if((n2>n3)&&(n3>n1)){
+            System.out.println("n2 is greater");
+        }
+        else {
+            System.out.println("n3 is greater");
+        }
+
+
+    }
+}
