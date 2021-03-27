74a4a604d070e8a8b1233f4566f8db28431dc19c
Yuvraj@162002 <yuvrajpareek44@gmail.com>
2021-03-27 12:09:26 -0700

    First task

--- src/com/task0/Main.java
+++ src/com/task0/Main.java
@@ -4,12 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("enter the value of integer 1");
+        System.out.println("enter the value of integer ");
         Scanner sc = new Scanner(System.in);
-        int  a1 = sc.nextInt();
-        System.out.println("enter the value of integer 2");
-        int a2 = sc.nextInt();
-        System.out.println("add two number");
-        System.out.println(a1+a2);
+        int  a = sc.nextInt();
+        System.out.println("enter number is:-"+a);
+        //System.out.println("enter the value of integer 2");
+        //int a2 = sc.nextInt();
+        //System.out.println("add two number");
+        //System.out.println(a1+a2);
     }
 }
