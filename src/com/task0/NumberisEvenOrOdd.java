6b34e52da5d3304d8129842f8df59355623531f9
Yuvraj@162002 <yuvrajpareek44@gmail.com>
2021-03-27 10:42:04 -0700

    Seventh task

---
+++ src/com/task0/NumberisEvenOrOdd.java
@@ -0,0 +1,20 @@
+package com.task0;
+
+import java.util.Scanner;
+
+public class NumberisEvenOrOdd {
+    public static void main(String[] args) {
+        System.out.println("Enter  number1");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+      // int n1 = num%2==0;
+         if(num%2==0) {
+             System.out.println("number is even");
+         }
+         else{
+             System.out.println("number is odd");
+         }
+
+
+    }
+}
