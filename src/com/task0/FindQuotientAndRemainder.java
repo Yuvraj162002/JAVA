7621d2699d9fa7124349b7de2369cbf777358081
Yuvraj@162002 <yuvrajpareek44@gmail.com>
2021-03-27 10:39:49 -0700

    Fifth task

---
+++ src/com/task0/FindQuotientAndRemainder.java
@@ -0,0 +1,20 @@
+package com.task0;
+
+import java.util.Scanner;
+
+public class FindQuotientAndRemainder {
+    public static void main(String[]args){
+        System.out.println("Enter  the value of dividend");
+        Scanner sc = new Scanner(System.in);
+        int dividend = sc.nextInt();
+        System.out.println("Enter  the value of divisor");
+        int divisor = sc.nextInt();
+
+        int quotient = (dividend/divisor);
+        System.out.println("Quotient is\t"+quotient);
+
+        int remainder = (dividend%divisor);
+        System.out.println("remainder is\t"+remainder);
+    }
+
+}
