c35e81992936832e16bb48d3c45e0c64e2f3d47f
Yuvraj@162002 <yuvrajpareek44@gmail.com>
2021-03-27 11:26:53 -0700

    Twelfth task

---
+++ src/com/task0/RemoveWhiteSpaces.java
@@ -0,0 +1,11 @@
+package com.task0;
+
+public class RemoveWhiteSpaces {
+    public static void main(String[]args){
+        String name = "WE WANT  TO    REMOVE ALL   WHITESPACE ";
+        System.out.println(name);
+        System.out.println(name.replaceAll("\\s",""));
+
+    }
+
+}
