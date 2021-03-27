2bac5069990c3e11882a26b1809dc144739f77f3
Yuvraj@162002 <yuvrajpareek44@gmail.com>
2021-03-27 10:37:06 -0700

    Fourth task

---
+++ src/com/task0/MultiplytwoNumber.java
@@ -0,0 +1,15 @@
+package com.task0;
+
+import java.util.Scanner;
+
+public class MultiplytwoNumber {
+    public  static  void main(String[]args){
+        System.out.println("Enter  number1");
+        Scanner sc = new Scanner(System.in);
+        float a = sc.nextFloat();
+        System.out.println("Enter  number2");
+        float b = sc.nextFloat();
+        System.out.println(a*b);
+    }
+
+}
