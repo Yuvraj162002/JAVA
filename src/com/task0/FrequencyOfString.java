15ef6114f5ea4c4184ebd1cc8a2db5ea834dcb86
Yuvraj@162002 <yuvrajpareek44@gmail.com>
2021-03-27 11:25:09 -0700

    Eleventh task

---
+++ src/com/task0/FrequencyOfString.java
@@ -0,0 +1,16 @@
+package com.task0;
+
+public class FrequencyOfString {
+    public  static void main(String[]args){
+        String str = "My Name Is Yuvarj Pareek";
+        char ch = 'a';
+        int frequency = 0;
+
+        for (int i = 0; i<str.length();i++){
+            if(ch == str.charAt(i)){
+                frequency++;
+            }
+        }
+        System.out.println("NO of frequency of a in this string is"+frequency);
+    }
+}
