d7c18d31a19d3ace8a7b2c2ad8fb13d73ae44067
Yuvraj@162002 <yuvrajpareek44@gmail.com>
2021-03-27 11:50:50 -0700

    Fourteenth task

---
+++ src/com/task0/RoundANumberToDecimal.java
@@ -0,0 +1,17 @@
+package com.task0;
+
+public class RoundANumberToDecimal {
+    public  static void main(String[]args){
+
+        double  d = 2.3444576;
+        System.out.println(d);
+
+        System.out.println("Math.floor(d)"+Math.floor(d));
+        System.out.println("Math.Ceiling(d)"+Math.ceil(d));
+        System.out.format("round is %.4f",d);
+
+
+
+
+    }
+}
