5561298bd6dd4aa5b08e0b39cfd243c3a93b3d09
Yuvraj@162002 <yuvrajpareek44@gmail.com>
2021-03-30 23:36:27 -0700

    Initial Commit

--- src/com/task0/String/StringContainASubstring.java
+++ src/com/task0/String/StringContainASubstring.java
@@ -6,8 +6,15 @@ public class StringContainASubstring {
     public static void main(String[] args) {
         Scanner sc =  new Scanner(System.in);
         System.out.println("Enter the string1");
-        String name1 = sc.next();
-        System.out.println(name1.substring(1));
+        String name1 = sc.nextLine();
+        if (name1.contains("pareek")){
+            System.out.println("yes the substring is present");
+        }
+        else {
+            System.out.println("no substring is not present");
+        }
+     //  System.out.println(name1.substring(1));
+        //System.out.println(name1.contains("yuvraj"));
 
     }
 }
