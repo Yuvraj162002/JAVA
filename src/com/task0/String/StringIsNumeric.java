1f00fb48c130a7b496e829f6f13296e61fb8b21c
Yuvraj@162002 <yuvrajpareek44@gmail.com>
2021-03-31 05:58:02 -0700

    Initial Commit

--- src/com/task0/String/StringIsNumeric.java
+++ src/com/task0/String/StringIsNumeric.java
@@ -1,15 +1,23 @@
 package com.task0.String;
 
+//// Here we do ..we convert string to numeric and if we enter the non-numeric string then it will throw a NumberformatException
+
 import java.util.Scanner;
 
 public class StringIsNumeric {
     public static void main(String[] args) {
         Scanner sc =  new Scanner(System.in);
-        System.out.println("Enter the string1");
-        String st= sc.next();
-      //  System.out.println(b1);
-        Integer.parseInt(st);
-        //ystem.out.println(str.H);
+        System.out.println("Enter the String");
+        String str = sc.next();
 
-    }
-}
+        int a = Integer.parseInt(str);
+        try {
+            System.out.println(a);
+           // throw  new NumberFormatException();
+        }catch (NumberFormatException e){
+            System.out.println(" the reason for this is");
+            System.out.println(e);
+        }
+
+        }
+        }
