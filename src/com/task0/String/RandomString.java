37833d94693fea30b47a941c6feaedc23bb9db86
Yuvraj@162002 <yuvrajpareek44@gmail.com>
2021-03-31 05:05:35 -0700

    Initial Commit

--- src/com/task0/String/RandomString.java
+++ src/com/task0/String/RandomString.java
@@ -5,8 +5,11 @@ import java.util.Random;
 public class RandomString {
     public static void main(String[] args) {
         System.out.println("Generate Random String");
+        String  st[] ={"A","b","c","d","e","f"};
         Random rd = new Random();
-        String cominput = rd.toString();
-        System.out.println(cominput);
+        int random = rd.nextInt(st.length);
+
+        System.out.println("the random number is"+st[random]);
+
     }
 }
