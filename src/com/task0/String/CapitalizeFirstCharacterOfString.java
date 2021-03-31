cac868d3604904097df55080e81ba224d33a1a7d
Yuvraj@162002 <yuvrajpareek44@gmail.com>
2021-03-31 04:57:10 -0700

    Initial Commit

--- src/com/task0/String/CapitalizeFirstCharacterOfString.java
+++ src/com/task0/String/CapitalizeFirstCharacterOfString.java
@@ -1,11 +1,23 @@
 package com.task0.String;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class CapitalizeFirstCharacterOfString {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the course name");
-        String course ="yuvraj";
+        System.out.println("Enter the name");
+        String name =sc.nextLine();
+
+       String firstletter = name.substring(0,1);
+       String remainingletter = name.substring(1,name.length());
+
+        firstletter = firstletter.toUpperCase(Locale.ROOT);
+
+         String result = firstletter + remainingletter;
+         System.out.println(result);
+
+      //  System.out.println(name.toUpperCase());
+
     }
 }
