e4f1b478f18875f52d0b4c96ac642e8f02b929fb
Yuvraj@162002 <yuvrajpareek44@gmail.com>
2021-03-31 00:30:19 -0700

    Initial Commit

---
+++ src/com/task0/String/CapitalizeFirstCharacterOfString.java
@@ -0,0 +1,11 @@
+package com.task0.String;
+
+import java.util.Scanner;
+
+public class CapitalizeFirstCharacterOfString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the course name");
+        String course ="yuvraj";
+    }
+}
--- src/com/task0/String/IterateEachCharacterOfString.java
+++ src/com/task0/String/IterateEachCharacterOfString.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class IterateEachCharacterOfString {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the course name");
-        String course ="yuvraj";
+        System.out.println("Enter the  name");
+        String name ="yuvraj";
         //String cousre[] = new String[6];
 
-        for (int i = 0;i<course.length();i++){
+        for (int i = 0;i<name.length();i++){
            // System.out.println("characetr is"+i);
-            char a = course.charAt(i);
+            char a = name.charAt(i);
             System.out.println(a);
         }
 
